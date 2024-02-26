7492399a40660cb8b5412faa7a2e118fd57ae405
Mihik <93090708+Mihik30@users.noreply.github.com>
2024-02-27 00:25:06 +0530

    Update Assignment_6B.py

--- CPPS_python/Assignment_6B.py
+++ CPPS_python/Assignment_6B.py
@@ -1,10 +1,15 @@
 #mihik shah
 #FY23B1D065
-#Assignment 6A
-
-# Python code to demonstrate the working of gcd()
-# importing "math" for mathematical operations
-import math
-# print output
+#Assignment 6B
+# Python code to demonstrate naive
+# method to compute gcd ( recursion )
+def hcf(a, b):
+  if(b == 0):
+    return a
+  else:
+    return hcf(b, a % b)
+a = 6086
+b = 498
+# prints output
 print("The gcd is : ", end="")
-print(math.gcd(180, 7856))
+print(hcf(6086, 498))
