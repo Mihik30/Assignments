5f2f1af1181989efda1e668516e5881b45340a21
Mihik <93090708+Mihik30@users.noreply.github.com>
2024-11-22 23:42:54 +0530

    Update 7 stack.cpp

--- DSA sem3/7 stack.cpp
+++ DSA sem3/7 stack.cpp
@@ -1,74 +1,89 @@
-#include<iostream> 
-using namespace std; 
- 
+#include <iostream>
+using namespace std;
 struct node 
- { 
-     int data; 
-     node*next; 
- }; 
-class stack 
- { 
-     node*head; 
-     node*tail; 
-    public: 
-     stack() 
-      { 
-          head=nullptr; 
-          tail=nullptr; 
-      } 
-    void push(int d) 
-     { 
-         node*newnode; 
-         newnode=new node(); 
-         newnode->data=d; 
-         newnode->next=nullptr; 
-         if(tail==nullptr) 
-          { 
-              head=newnode; 
-          } 
-         else 
-          { 
-              tail->next=newnode; 
-          } 
-              tail=newnode; 
-     } 
-    void pop() 
-     { 
-         node*temp; 
-         temp=head; 
-         while(temp->next->next!=nullptr) 
-          { 
-              temp=temp->next; 
-          } 
-         if(head==tail) 
-          { 
-              delete head; 
-              head=nullptr; 
-              tail=nullptr; 
-              return; 
-          } 
-         node*temp2; 
-         temp2=temp->next; 
-         temp->next=nullptr; 
-         delete temp2; 
-     } 
-    void display() 
-     { 
-         node*temp; 
-         temp=head; 
-         while(temp!=nullptr) 
-          { 
-              cout<<temp->data<<endl; 
-              temp=temp->next; 
-} 
-} 
-}; 
+{
+    int data;
+    node *next;
+    node(int s) 
+    {
+        data =s;
+        next =NULL;
+    }
+};
+class browes
+{
+    public:
+    node *top;
+    browes()
+    {
+        top=NULL;
+    }
+    void add(int y)
+    {
+        node *new_node =new node (y);
+        new_node->next=top;
+        top=new_node;
+        cout<<"PAGE PUSHED"<<y<<endl;
+    }
+    void viewprev()
+    {
+        if(top==NULL)
+        {
+            cout<<"NO PREV PAGE"<<endl;
+        }
+        if(top->next==NULL)
+        {
+            cout<<"NO PREV PAGE"<<endl;
+        }
+        else
+        {
+            node *temp=top;
+            top=top->next;
+            delete(temp);
+            cout<<"previous "<<top->data;
+        }
+    }
+    
+    void view()
+    {
+        if(top==NULL)
+        {
+            cout<<"NO CURRENT PAGE";
+        }
+        else
+        {
+            cout<<" CURRENT PAGE"<<top->data<<endl;
+        }
+    }
+    
+    void checkempty()
+    {
+        if(top==NULL)
+        {
+             cout<<"EMPTY";
+        }
+        else
+        {
+            cout<<"NOT EMPTY";
+        }
+    }
+
+};
+
 int main() 
-{ 
-} 
-stack s; 
-s.push(2); 
-s.push(4); 
-s.push(7); 
-s.pop(); 
-s.display(); 
+{
+    browes aa;
+    // cout<<"------"<<endl;
+    aa.add(8);
+    aa.add(6);
+    aa.view();
+    // cout<<"------"<<endl;
+    // cout<<"------"<<endl;
+    aa.viewprev();
+    // cout<<"------"<<endl;
+    aa.view();
+    // cout<<"------"<<endl;
+    aa.checkempty();
+
+    return 0;
+}
